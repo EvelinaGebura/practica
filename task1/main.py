ab6af56519aff76b103d1fefc8701568731dfbe0
EvelinaGebura <86943250+EvelinaGebura@users.noreply.github.com>
2021-07-12 15:57:10 +0300

    Add files via upload

--- task1/main.py
+++ task1/main.py
@@ -11,8 +11,11 @@ print("Числа з рядка:", nums)
 WithLarge = ' '.join(word[0].upper() + word[1:-1] + word[-1:].upper() for word in word.split())
 print("\nРядок після змін:", WithLarge)
 
-nums.remove(max(nums))
-numberIndex = [nums[i]**i for i in range(0,len(nums))]
+if not nums:
+    print(False)
+else:
+    nums.remove(max(nums))
+    numberIndex = [nums[i]**i for i in range(0,len(nums))]
+    print("Масив чисел в степені за їхнім індексом:", numberIndex)
 
-print("Масив чисел в степені за їхнім індексом:", numberIndex)
 print("\n")
